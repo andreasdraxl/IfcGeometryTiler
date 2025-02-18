38040729264a12452733509b0459a7ba3ad53a87
andreasdraxl <andreas_draxl_@live.de>
2025-02-18 09:36:47 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./streaming-settings";
+//export { StreamedAsset as BaseStreamedAsset, StreamedGeometries as BaseStreamedGeometries } from "./base-types";
+export * from "./async-event";
+export * from "./civil-reader";
+export * from "./ifc-metadata-reader";
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+import * as path from "path";
+import * as packageJson from "./package.json";
+
+export default defineConfig({
+  build: {
+    outDir: "./dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: Object.keys(packageJson.peerDependencies),
+      output: {
+        globals: {
+          three: "THREE",
+          "@thatopen/ui-components": "BUI",
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      include: ["./src"],
+      exclude: ["./src/**/example.ts", "./src/**/*.test.ts"],
+    }),
+  ],
+});
